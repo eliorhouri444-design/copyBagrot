a21297d9201bab46e4155d85cf84e16f0821d1a3
אליאור חורי <eliorhouri444@gmail.com>
2025-11-23 12:22:25 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -394,7 +394,7 @@ export default function HomePage() {
       icon: Target,
       title: "מבחן טעויות מבגרויות",
       description: "בגרות מותאמת אישית על בסיס הטעויות שלך",
-      color: "from-orange-500 to-red-600",
+      color: "from-blue-500 to-cyan-600",
       onClick: () => navigate(createPageUrl("CustomWeakExam")),
       delay: 0.45,
       isPremium: true
