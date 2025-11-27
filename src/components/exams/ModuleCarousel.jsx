74f48898cbd1f4316af226ad9266043b7b4a11b9
אליאור חורי <eliorhouri444@gmail.com>
2025-11-27 19:05:04 +0000

    File changes

--- src/components/exams/ModuleCarousel.jsx
+++ src/components/exams/ModuleCarousel.jsx
@@ -453,7 +453,7 @@ export default function ModuleCarousel({
                         {isPremium === true ?
                   <Button
                     onClick={() => handleModuleClick(currentModule)}
-                    variant="outline" className="bg-[#ffffff] text-blue-500 px-4 py-2 font-semibold rounded-[14px] inline-flex items-center justify-center gap-2 whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 shadow-sm w-full h-10 border-2 border-blue-500 hover:bg-white hover:text-blue-500">
+                    variant="outline" className="bg-[#ffffff] text-black px-4 py-2 font-semibold rounded-[14px] inline-flex items-center justify-center gap-2 whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 shadow-sm w-full h-10 border-2 border-black hover:bg-white hover:text-black">
 
 
                             בחר בגרות ספציפית (מעל 100 בגרויות)
