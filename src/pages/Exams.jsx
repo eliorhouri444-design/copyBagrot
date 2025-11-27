ec63d3ff841dc05f271b110bf0b3b7cc51d6e0d6
אליאור חורי <eliorhouri444@gmail.com>
2025-11-27 18:06:47 +0000

    File changes

--- src/pages/Exams.jsx
+++ src/pages/Exams.jsx
@@ -830,7 +830,7 @@ export default function ExamsPage() {
                               }} className="bg-blue-500 text-[11px] px-4 py-2 font-bold rounded-[14px] whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 shadow hover:bg-blue-600 from-blue-500 to-blue-600 hover:from-orange-600 hover:to-red-700 w-full h-9 flex items-center justify-center gap-2">
 
                                 <Target className="text-white" />
-                                תרגל {wrongCount} טעויות
+                                תקן {wrongCount} טעויות
                               </Button>
                             </div>);
 
@@ -1406,7 +1406,7 @@ export default function ExamsPage() {
                           {isPremium ?
                           <>
                               <Target className="w-3 h-3" />
-                              <span>תרגל {wrongCount} טעויות</span>
+                              <span>תקן {wrongCount} טעויות</span>
                             </> :
 
                           <>
