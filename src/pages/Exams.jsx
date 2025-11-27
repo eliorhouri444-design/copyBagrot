821183de35e6446b50ee566d81da7a31a506f7e8
אליאור חורי <eliorhouri444@gmail.com>
2025-11-27 19:18:02 +0000

    Manual code change

--- src/pages/Exams.jsx
+++ src/pages/Exams.jsx
@@ -844,7 +844,8 @@ export default function ExamsPage() {
               <motion.div whileHover={{ scale: 1.02 }} whileTap={{ scale: 0.98 }}>
                     <Button
                   onClick={() => setShowAllExams(true)}
-                  className="bg-[#3B82F6] text-white text-[13px] mt-2 px-4 py-2 font-bold rounded-[14px] inline-flex items-center justify-center gap-2 whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-blue-400 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 shadow w-full h-10 hover:bg-[#2563EB] active:bg-[#1E40AF]">
+                  variant="outline" className="bg-[#ffffff] text-blue-500 mt-2 px-4 py-2 text-sm font-semibold rounded-[14px] inline-flex items-center justify-center gap-2 whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 shadow-sm hover:text-blue-500 w-full h-10 border-2 border-blue-500 hover:bg-[#F5F8FF]">
+
 
                       צפה בכל הבגרויות ({examAttempts.length})
                     </Button>
